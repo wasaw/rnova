c4f77b0ceee1808562ac7ed11629ae920b844cf9
wasaw <merenkovalexander@gmail.com>
2021-10-26 17:16:06 +0300

    Добавлен поиск по докторам и специальностям

--- rnova/Controller/Record/RecordController.swift
+++ rnova/Controller/Record/RecordController.swift
@@ -16,6 +16,15 @@ class RecordController: UIViewController {
     private var quantityProfession = [Int: Int]()
     private let insents = UIEdgeInsets(top: 20, left: 0, bottom: 0, right: 0)
     private var isDoctorChoice = true
+    
+    private var searchController = UISearchController(searchResultsController: nil)
+    private var searchBarIsEmpty: Bool {
+        guard let text = searchController.searchBar.text else { return false}
+        return text.isEmpty
+    }
+    
+    private var filteredSearchResultDoctors = [Doctors]()
+    private var filteredSearchResultProfessions = [Professions]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,24 +35,24 @@ class RecordController: UIViewController {
             self.doctorsData = DataLoader(urlMethod: "&method=getUsers", urlParameter: "").doctorsData
             self.professionsData = DataLoader(urlMethod: "&method=getProfessions", urlParameter: "").professionsData
             self.collectionView?.reloadData()
-            self.countingNumberOfProfessions()
+            self.countingQuantityOfProfessions()
         }
-    
-        let statusBar = UIView()
-        statusBar.frame = UIApplication.shared.statusBarFrame
-        statusBar.backgroundColor = .systemOrange
-        UIApplication.shared.keyWindow?.addSubview(statusBar)
-        navigationController?.navigationBar.backgroundColor = .systemOrange
-        navigationController?.preferredStatusBarStyle
-        navigationItem.title = "Запись"
-        }
+    }
 
     
     func configureUI() {
+        configureSegmentedControl()
+        configureStatusBar()
+        configureSearchBar()
+        configureCollectionView()
+    }
+    
+    func configureSegmentedControl() {
         view.addSubview(segmentedControl)
+        
         segmentedControl.translatesAutoresizingMaskIntoConstraints = false
         segmentedControl.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20).isActive = true
-        segmentedControl.topAnchor.constraint(equalTo: view.topAnchor, constant: 120).isActive = true
+        segmentedControl.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 30).isActive = true
         segmentedControl.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20).isActive = true
         segmentedControl.heightAnchor.constraint(equalToConstant: 30).isActive = true
         
@@ -53,11 +62,52 @@ class RecordController: UIViewController {
         segmentedControl.backgroundColor = .systemOrange
         
         segmentedControl.addTarget(self, action: #selector(didTapSegment), for: .valueChanged)
-        
-        collectonViewSetup()
     }
     
-    func countingNumberOfProfessions() {
+    func configureStatusBar() {
+        let statusBar = UIView()
+        statusBar.frame = UIApplication.shared.statusBarFrame
+        statusBar.backgroundColor = .systemOrange
+        UIApplication.shared.keyWindow?.addSubview(statusBar)
+        navigationController?.navigationBar.backgroundColor = .systemOrange
+        navigationController?.preferredStatusBarStyle
+        navigationItem.title = "Запись"
+    }
+    
+    func configureSearchBar() {
+        navigationItem.searchController = searchController
+
+        searchController.searchResultsUpdater = self
+        searchController.searchBar.delegate = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.placeholder = "Поиск"
+        searchController.searchBar.barTintColor = .white
+        collectionView?.addSubview(searchController.searchBar)
+        
+        searchController.searchBar.translatesAutoresizingMaskIntoConstraints = false
+        searchController.searchBar.heightAnchor.constraint(equalToConstant: 40).isActive = true
+//        searchController.searchBar.widthAnchor.constraint(equalToConstant: view.frame.width).isActive = true
+    }
+    
+    func configureCollectionView() {
+        collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+        guard let collectionView = collectionView else { return }
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.register(UINib(nibName: RecordViewCell.identifite, bundle: nil), forCellWithReuseIdentifier: RecordViewCell.identifite)
+        collectionView.register(UINib(nibName: ProfessionViewCell.identifire, bundle: nil), forCellWithReuseIdentifier: ProfessionViewCell.identifire)
+        
+        view.addSubview(collectionView)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        collectionView.topAnchor.constraint(equalTo: segmentedControl.bottomAnchor, constant:  20).isActive = true
+        collectionView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        
+        collectionView.backgroundColor = .white
+    }
+    
+    func countingQuantityOfProfessions() {
         for item in 0..<professionsData.count {
             var quantity = 0
             let id = professionsData[item].id
@@ -85,32 +135,24 @@ class RecordController: UIViewController {
             collectionView?.reloadData()
         }
     }
-    
-    func collectonViewSetup() {
-        collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
-        guard let collectionView = collectionView else { return }
-        collectionView.delegate = self
-        collectionView.dataSource = self
-        collectionView.register(UINib(nibName: RecordViewCell.identifite, bundle: nil), forCellWithReuseIdentifier: RecordViewCell.identifite)
-        collectionView.register(UINib(nibName: ProfessionViewCell.identifire, bundle: nil), forCellWithReuseIdentifier: ProfessionViewCell.identifire)
-        
-        view.addSubview(collectionView)
-        collectionView.translatesAutoresizingMaskIntoConstraints = false
-        collectionView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
-        collectionView.topAnchor.constraint(equalTo: segmentedControl.bottomAnchor, constant:  20).isActive = true
-        collectionView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
-        collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
-    }
 }
 
 //    MARK: -- extension
 extension RecordController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         if doctorsData.count != 0 && isDoctorChoice {
-            return doctorsData.count
+            if searchBarIsEmpty {
+                return doctorsData.count
+            } else {
+                return filteredSearchResultDoctors.count
+            }
         }
         if professionsData.count != 0 && !isDoctorChoice {
-            return professionsData.count
+            if searchBarIsEmpty {
+                return professionsData.count
+            } else {
+                return filteredSearchResultProfessions.count
+            }
         }
         return 0
     }
@@ -119,22 +161,36 @@ extension RecordController: UICollectionViewDataSource {
         guard let cellDoctor = collectionView.dequeueReusableCell(withReuseIdentifier: RecordViewCell.identifite, for: indexPath) as? RecordViewCell else { return UICollectionViewCell() }
         guard let cellProfession = collectionView.dequeueReusableCell(withReuseIdentifier: ProfessionViewCell.identifire, for: indexPath) as? ProfessionViewCell else { return UICollectionViewCell()}
         if isDoctorChoice {
-            if doctorsData.count != 0 {
-                if doctorsData[indexPath.row].avatar_small != nil {
-                    cellDoctor.profileImageView.downloaded(from: doctorsData[indexPath.row].avatar_small!)
+            if searchBarIsEmpty {
+                if doctorsData.count != 0 {
+                    if doctorsData[indexPath.row].avatar_small != nil {
+                        cellDoctor.profileImageView.downloaded(from: doctorsData[indexPath.row].avatar_small!)
+                    }
+                    cellDoctor.surnameLabel.text = doctorsData[indexPath.row].name
+                    cellDoctor.professionLabel.text = doctorsData[indexPath.row].profession_titles
                 }
-                cellDoctor.surnameLabel.text = doctorsData[indexPath.row].name
-                cellDoctor.professionLabel.text = doctorsData[indexPath.row].profession_titles
+            } else {
+                if doctorsData.count != 0 {
+                    if filteredSearchResultDoctors[indexPath.row].avatar_small != nil {
+                        cellDoctor.profileImageView.downloaded(from: filteredSearchResultDoctors[indexPath.row].avatar_small!)
+                    }
+                    cellDoctor.surnameLabel.text = filteredSearchResultDoctors[indexPath.row].name
+                    cellDoctor.professionLabel.text = filteredSearchResultDoctors[indexPath.row].profession_titles
+                }
             }
+            
             return cellDoctor
         } else {
-//            let quantityProfession = countingNumberOfProfessions(id: professionsData[indexPath.row].id)
-            guard let quantity = quantityProfession[professionsData[indexPath.row].id] else { return UICollectionViewCell() }
-            cellProfession.specialtyLabel.text = "\(professionsData[indexPath.row].name) (\(quantity))"
+            if searchBarIsEmpty {
+                guard let quantity = quantityProfession[professionsData[indexPath.row].id] else { return UICollectionViewCell() }
+                cellProfession.specialtyLabel.text = "\(professionsData[indexPath.row].name) (\(quantity))"
+            } else {
+                guard let quantity = quantityProfession[filteredSearchResultProfessions[indexPath.row].id] else { return UICollectionViewCell() }
+                cellProfession.specialtyLabel.text = "\(filteredSearchResultProfessions[indexPath.row].name) (\(quantity))"
+            }
+            
             return cellProfession
         }
-        
-//        return UICollectionViewCell()
     }
 }
 
@@ -160,5 +216,22 @@ extension RecordController: UICollectionViewDelegateFlowLayout {
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
         return insents
+    }
+}
+
+extension RecordController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        if !searchBarIsEmpty {
+            if isDoctorChoice {
+                filteredSearchResultDoctors = doctorsData.filter({ (list: Doctors) -> Bool in
+                    return list.name.lowercased().contains(searchController.searchBar.text?.lowercased() ?? "")
+                })
+            } else {
+                filteredSearchResultProfessions = professionsData.filter({ (list: Professions) -> Bool in
+                    return list.name.lowercased().contains(searchController.searchBar.text?.lowercased() ?? "")
+                })
+            }
+        }
+        collectionView?.reloadData()
     }
 }
--- rnova/Record/ViewController.swift
+++ rnova/Record/ViewController.swift
@@ -1,208 +1,208 @@
+////
+////  ViewController.swift
+////  rnova
+////
+////  Created by Александр Меренков on 1/20/21.
+////
 //
-//  ViewController.swift
-//  rnova
+//import UIKit
 //
-//  Created by Александр Меренков on 1/20/21.
+//class ViewController: UIViewController {
 //
-
-import UIKit
-
-class ViewController: UIViewController {
-
-    var doctorVC: UIView!
-    var specialtyVC: UIView!
-    var doctorsData = [Doctors]()
-    var professionsData = [Professions]()
-
-    @IBOutlet weak var collectionView: UICollectionView!
-    @IBOutlet weak var layout: UICollectionViewFlowLayout!
-
-    let cellID = "RecordViewCell"
-    let insents = UIEdgeInsets(top: 70, left: 0, bottom: 0, right: 0)
-
-    private var filteredSearchResultDoctors = [Doctors]()
-    private var filteredSearchResultProfessions = [Professions]()
-    private var searchController = UISearchController(searchResultsController: nil)
-    private var searchBarIsEmpty: Bool {
-        guard let text = searchController.searchBar.text else { return false }
-        return text.isEmpty
-    }
-    private var isFiltering: Bool {
-        return searchController.isActive && !searchBarIsEmpty
-    }
-
-    var checkTapSegment = true
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        let activityIndicator = UIActivityIndicatorView(style: UIActivityIndicatorView.Style.medium)
-        activityIndicator.center = view.center
-        activityIndicator.startAnimating()
-        view.addSubview(activityIndicator)
-
-        DispatchQueue.main.async {
-            self.doctorsData = DataLoader(urlMethod: "&method=getUsers", urlParameter: "").doctorsData
-            self.professionsData = DataLoader(urlMethod: "&method=getProfessions", urlParameter: "").professionsData
-            activityIndicator.stopAnimating()
-            self.collectionView.reloadData()
-        }
-
-        collectonViewSetup()
-
-        searchController.searchResultsUpdater = self
-        searchController.obscuresBackgroundDuringPresentation = false
-        searchController.searchBar.placeholder = "Поиск"
-        searchController.searchBar.barTintColor = .white
-        searchController.searchBar.frame = CGRect(x: 0, y: 0, width:  collectionView.bounds.width - 5, height: 50)
-        collectionView.addSubview(searchController.searchBar)
-
-        definesPresentationContext = true
-    }
-
-    func collectonViewSetup() {
-        collectionView.delegate = self
-        collectionView.dataSource = self
-        collectionView.register(UINib(nibName: cellID, bundle: nil), forCellWithReuseIdentifier: cellID)
-    }
-
-    @IBAction func didTapSegment(segment: UISegmentedControl) {
-        if segment.selectedSegmentIndex == 0 {
-            checkTapSegment = true
-            collectionView.reloadData()
-        }else {
-            checkTapSegment = false
-            collectionView.reloadData()
-        }
-    }
-}
-
-//    MARK: -- extension
-extension ViewController: UICollectionViewDataSource {
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        if checkTapSegment {
-            if isFiltering {
-                return filteredSearchResultDoctors.count
-            }else {
-                return doctorsData.count
-            }
-        }else {
-            if isFiltering {
-                return filteredSearchResultProfessions.count
-            }else {
-                return professionsData.count
-            }
-        }
-    }
-
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = UICollectionViewCell()
-//        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellID, for: indexPath) as? RecordViewCell else {
-//            return UICollectionViewCell()
+//    var doctorVC: UIView!
+//    var specialtyVC: UIView!
+//    var doctorsData = [Doctors]()
+//    var professionsData = [Professions]()
+//
+//    @IBOutlet weak var collectionView: UICollectionView!
+//    @IBOutlet weak var layout: UICollectionViewFlowLayout!
+//
+//    let cellID = "RecordViewCell"
+//    let insents = UIEdgeInsets(top: 70, left: 0, bottom: 0, right: 0)
+//
+//    private var filteredSearchResultDoctors = [Doctors]()
+//    private var filteredSearchResultProfessions = [Professions]()
+////    private var searchController = UISearchController(searchResultsController: nil)
+////    private var searchBarIsEmpty: Bool {
+//////        guard let text = searchController.searchBar.text else { return false }
+////        return text.isEmpty
+////    }
+////    private var isFiltering: Bool {
+//////        return searchController.isActive && !searchBarIsEmpty
+////    }
+//
+//    var checkTapSegment = true
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        let activityIndicator = UIActivityIndicatorView(style: UIActivityIndicatorView.Style.medium)
+//        activityIndicator.center = view.center
+//        activityIndicator.startAnimating()
+//        view.addSubview(activityIndicator)
+//
+//        DispatchQueue.main.async {
+//            self.doctorsData = DataLoader(urlMethod: "&method=getUsers", urlParameter: "").doctorsData
+//            self.professionsData = DataLoader(urlMethod: "&method=getProfessions", urlParameter: "").professionsData
+//            activityIndicator.stopAnimating()
+//            self.collectionView.reloadData()
 //        }
 //
-//        if doctorsData[indexPath.row].avatar_small != nil {
-//            cell.profileImageView.downloaded(from: doctorsData[indexPath.row].avatar_small!)
-//        }
+//        collectonViewSetup()
 //
-//        if checkTapSegment {
-//            cell.profileImageView.isHidden = false
-//            cell.label.frame = CGRect(x: 120, y: 20, width: cell.frame.width - 145, height: 50)
-//            cell.label.lineBreakMode = .byWordWrapping
-//            cell.label.numberOfLines = 2
-//            if isFiltering {
-//                cell.label.text = filteredSearchResultDoctors[indexPath.row].name
-//            }else {
-//                cell.label.text = doctorsData[indexPath.row].name
-//            }
+////        searchController.searchResultsUpdater = self
+////        searchController.obscuresBackgroundDuringPresentation = false
+////        searchController.searchBar.placeholder = "Поиск"
+////        searchController.searchBar.barTintColor = .white
+////        searchController.searchBar.frame = CGRect(x: 0, y: 0, width:  collectionView.bounds.width - 5, height: 50)
+////        collectionView.addSubview(searchController.searchBar)
+//
+//        definesPresentationContext = true
+//    }
+//
+//    func collectonViewSetup() {
+//        collectionView.delegate = self
+//        collectionView.dataSource = self
+//        collectionView.register(UINib(nibName: cellID, bundle: nil), forCellWithReuseIdentifier: cellID)
+//    }
+//
+//    @IBAction func didTapSegment(segment: UISegmentedControl) {
+//        if segment.selectedSegmentIndex == 0 {
+//            checkTapSegment = true
+//            collectionView.reloadData()
 //        }else {
-//            //counting the number of doctors by profession
-//            let id = professionsData[indexPath.row].id
-//            var commonCount = 0
-//            for item in 0...doctorsData.count - 1 {
-//                guard let str = doctorsData[item].profession else { continue }
-//                if str.count > 0 {
-//                    for i in 0...str.count - 1 {
-//                        if id == Int(str[i]) {
-//                            commonCount += 1
-//                        }
-//                    }
-//                }
-//            }
-//
-//            cell.profileImageView.isHidden = true
-//            cell.label.frame = CGRect(x: 15, y: 5, width: cell.frame.width - 15, height: 50)
-//            if isFiltering {
-//                cell.label.text = filteredSearchResultProfessions[indexPath.row].name
-//            }else {
-//                cell.label.text = "\(professionsData[indexPath.row].name) (\(commonCount))"
-//            }
+//            checkTapSegment = false
+//            collectionView.reloadData()
 //        }
-        return cell
-    }
-
-
-}
-
-extension ViewController: UICollectionViewDelegate {
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        let id: Int
-        let vc: UIViewController
-        if checkTapSegment {
-            if isFiltering {
-                id = filteredSearchResultDoctors[indexPath.row].id
-                vc = DoctorChoiceViewController(id: id)
-            }else {
-                id = doctorsData[indexPath.row].id
-                vc = DoctorChoiceViewController(id: id)
-            }
-        }else {
-            if isFiltering {
-                id = filteredSearchResultProfessions[indexPath.row].id
-                vc = SpecialtyChoiceViewController(id: id)
-            }else {
-                id = professionsData[indexPath.row].id
-                vc = SpecialtyChoiceViewController(id: id)
-            }
-        }
-        navigationController?.pushViewController(vc, animated: true)
-    }
-}
-
-extension ViewController: UICollectionViewDelegateFlowLayout {
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let width: CGFloat = collectionView.frame.width - 20
-        let height: CGFloat
-        if checkTapSegment {
-            height = width / 4
-        }else {
-            height = 60
-        }
-        return CGSize(width: width, height: height)
-    }
-
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        return 20
-    }
-
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-        return insents
-    }
-}
-
-extension ViewController: UISearchResultsUpdating {
-    func updateSearchResults(for searchController: UISearchController) {
-        filterContentForSearchText(searchController.searchBar.text!)
-    }
-
-    private func filterContentForSearchText(_ searchText: String) {
-        if checkTapSegment {
-            filteredSearchResultDoctors = doctorsData.filter({ (list: Doctors) -> Bool in
-                return list.name.lowercased().contains(searchText.lowercased())
-            })
-        }else {
-            filteredSearchResultProfessions = professionsData.filter({ (list: Professions) -> Bool in
-                return list.name.lowercased().contains(searchText.lowercased())
-            })
-        }
-        collectionView.reloadData()
-    }
-}
+//    }
+//}
+//
+////    MARK: -- extension
+//extension ViewController: UICollectionViewDataSource {
+//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+//        if checkTapSegment {
+////            if isFiltering {
+////                return filteredSearchResultDoctors.count
+////            }else {
+////                return doctorsData.count
+////            }
+//        }else {
+////            if isFiltering {
+////                return filteredSearchResultProfessions.count
+////            }else {
+////                return professionsData.count
+////            }
+//        }
+//    }
+//
+//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+//        let cell = UICollectionViewCell()
+////        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellID, for: indexPath) as? RecordViewCell else {
+////            return UICollectionViewCell()
+////        }
+////
+////        if doctorsData[indexPath.row].avatar_small != nil {
+////            cell.profileImageView.downloaded(from: doctorsData[indexPath.row].avatar_small!)
+////        }
+////
+////        if checkTapSegment {
+////            cell.profileImageView.isHidden = false
+////            cell.label.frame = CGRect(x: 120, y: 20, width: cell.frame.width - 145, height: 50)
+////            cell.label.lineBreakMode = .byWordWrapping
+////            cell.label.numberOfLines = 2
+////            if isFiltering {
+////                cell.label.text = filteredSearchResultDoctors[indexPath.row].name
+////            }else {
+////                cell.label.text = doctorsData[indexPath.row].name
+////            }
+////        }else {
+////            //counting the number of doctors by profession
+////            let id = professionsData[indexPath.row].id
+////            var commonCount = 0
+////            for item in 0...doctorsData.count - 1 {
+////                guard let str = doctorsData[item].profession else { continue }
+////                if str.count > 0 {
+////                    for i in 0...str.count - 1 {
+////                        if id == Int(str[i]) {
+////                            commonCount += 1
+////                        }
+////                    }
+////                }
+////            }
+////
+////            cell.profileImageView.isHidden = true
+////            cell.label.frame = CGRect(x: 15, y: 5, width: cell.frame.width - 15, height: 50)
+////            if isFiltering {
+////                cell.label.text = filteredSearchResultProfessions[indexPath.row].name
+////            }else {
+////                cell.label.text = "\(professionsData[indexPath.row].name) (\(commonCount))"
+////            }
+////        }
+//        return cell
+//    }
+//
+//
+//}
+//
+//extension ViewController: UICollectionViewDelegate {
+//    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+//        let id: Int
+//        let vc: UIViewController
+//        if checkTapSegment {
+////            if isFiltering {
+////                id = filteredSearchResultDoctors[indexPath.row].id
+////                vc = DoctorChoiceViewController(id: id)
+////            }else {
+////                id = doctorsData[indexPath.row].id
+////                vc = DoctorChoiceViewController(id: id)
+////            }
+//        }else {
+////            if isFiltering {
+////                id = filteredSearchResultProfessions[indexPath.row].id
+////                vc = SpecialtyChoiceViewController(id: id)
+////            }else {
+////                id = professionsData[indexPath.row].id
+////                vc = SpecialtyChoiceViewController(id: id)
+////            }
+//        }
+////        navigationController?.pushViewController(vc, animated: true)
+//    }
+//}
+//
+//extension ViewController: UICollectionViewDelegateFlowLayout {
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+//        let width: CGFloat = collectionView.frame.width - 20
+//        let height: CGFloat
+//        if checkTapSegment {
+//            height = width / 4
+//        }else {
+//            height = 60
+//        }
+//        return CGSize(width: width, height: height)
+//    }
+//
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+//        return 20
+//    }
+//
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+//        return insents
+//    }
+//}
+//
+//extension ViewController: UISearchResultsUpdating {
+//    func updateSearchResults(for searchController: UISearchController) {
+//        filterContentForSearchText(searchController.searchBar.text!)
+//    }
+//
+//    private func filterContentForSearchText(_ searchText: String) {
+//        if checkTapSegment {
+//            filteredSearchResultDoctors = doctorsData.filter({ (list: Doctors) -> Bool in
+//                return list.name.lowercased().contains(searchText.lowercased())
+//            })
+//        }else {
+//            filteredSearchResultProfessions = professionsData.filter({ (list: Professions) -> Bool in
+//                return list.name.lowercased().contains(searchText.lowercased())
+//            })
+//        }
+//        collectionView.reloadData()
+//    }
+//}
