912d2470640a03a7fdc9e6f98907d04df26532db
wasaw <merenkovalexander@gmail.com>
2021-11-30 17:45:42 +0300

    Добавлен LoginController

--- rnova/Controller/Profile/LoginController.swift
+++ rnova/Controller/Profile/LoginController.swift
@@ -9,9 +9,58 @@ import UIKit
 
 class LoginController: UIViewController {
     
+    private let loginView = LoginView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .green
+        navigationItem.title = "Вход"
+        navigationController?.navigationBar.backgroundColor = .systemOrange
+        
+        configuretionView()
+        
+        view.backgroundColor = .white
     }
+    
+    func configuretionView() {
+        view.addSubview(loginView)
+        
+        loginView.delegate = self
+        loginView.translatesAutoresizingMaskIntoConstraints = false
+        loginView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        loginView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        loginView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        loginView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+    }
+    
+    func alert(fields: String) {
+        let message = "Не заполнены следующие поля: " + fields
+        let alert = UIAlertController(title: "Внимание!", message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Ок", style: .default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    func isValidPhoneNumber(number: String?) -> Bool {
+        guard let number = number else { return false }
+        let regEx = "^\\+7\\d{3}-\\d{3}-\\d{4}$"
+        let phoneCheck = NSPredicate(format: "SELF MATCHES %@", regEx)
+        return phoneCheck.evaluate(with: number)
+    }
+}
+
+//MARK: - extension
+extension LoginController: SendPhoneNumberProtocol {
+    func sendProtocol(phoneNumber: String) {
+        if isValidPhoneNumber(number: phoneNumber) {
+            print("DEBUG: Saving")
+        }else {
+            alert(fields: """
+                  Номер телефона.
+                  Формат ввода номера
+                  +7XXX-XXX-XXXX
+                  """)
+        }
+    }
+    
+    
 }
---
+++ rnova/View/Profile/LoginView.swift
@@ -0,0 +1,66 @@
+//
+//  LoginView.swift
+//  rnova
+//
+//  Created by Александр Меренков on 11/26/21.
+//
+
+import UIKit
+
+protocol SendPhoneNumberProtocol {
+    func sendProtocol(phoneNumber: String)
+}
+
+class LoginView: UIView {
+    private let phoneNumberField: UITextField = {
+        let tf = UITextField()
+        tf.placeholder = "Номер телефона"
+        tf.clearButtonMode = .always
+        tf.clearsOnBeginEditing = true
+        return tf
+    }()
+    
+    private let enterButton = View().button
+    
+    var delegate: SendPhoneNumberProtocol?
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        addSubview(phoneNumberField)
+        addSubview(enterButton)
+        
+        enterButton.backgroundColor = UIColor.systemOrange
+        enterButton.setTitle("Войти", for: .normal)
+        enterButton.addTarget(self, action: #selector(entering), for: .touchUpInside)
+        
+        backgroundColor = .white
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        phoneNumberField.translatesAutoresizingMaskIntoConstraints = false
+        phoneNumberField.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 20).isActive = true
+        phoneNumberField.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor, constant: 30).isActive = true
+        phoneNumberField.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -20).isActive = true
+        phoneNumberField.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        phoneNumberField.addLine()
+        
+        enterButton.translatesAutoresizingMaskIntoConstraints = false
+        enterButton.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 10).isActive = true
+        enterButton.topAnchor.constraint(equalTo: phoneNumberField.bottomAnchor, constant: 30).isActive = true
+        enterButton.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -10).isActive = true
+        enterButton.heightAnchor.constraint(equalToConstant: 60).isActive = true
+    }
+    
+    @objc func entering() {
+        guard let phoneNumber = phoneNumberField.text else { return }
+        delegate?.sendProtocol(phoneNumber: phoneNumber)
+    }
+    
+}
